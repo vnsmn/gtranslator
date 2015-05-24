fa7e4f1af524f444013dbfdee3e2de1a8f1be309
vns <01_kilimandjaro@ukr.net>
2015-05-24 21:18:55 +0300

    --

---
+++ src/main/java/gtranslator/HistoryHelper.java
@@ -0,0 +1,55 @@
+package gtranslator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+public class HistoryHelper {
+	public final static HistoryHelper INSTANCE = new HistoryHelper();
+	private Properties rawHis = new Properties();
+	private Properties wordHis = new Properties();
+
+	private HistoryHelper() {}
+	
+	public void load(File rawHisFile, File wordHisFile) throws FileNotFoundException, IOException {
+		if (rawHisFile.exists())
+			try (FileInputStream in = new FileInputStream(rawHisFile)) {
+				rawHis.loadFromXML(in);
+			}		
+		if (wordHisFile.exists())
+			try (FileInputStream in = new FileInputStream(wordHisFile)) {
+				wordHis.loadFromXML(in);
+			}
+	}
+	
+	public void writeRaw(String key, String value) {
+		rawHis.put(key, value);
+	}
+	
+	public void writeWord(String key, String value) {
+		wordHis.put(key, value);
+	}
+	
+	public String readRaw(String key) {
+		return rawHis.getProperty(key, null);
+	}
+	
+	public void save(File rawHisFile, File wordHisFile) throws FileNotFoundException, IOException {
+		if (!rawHisFile.exists()) {
+			rawHisFile.createNewFile();
+		}
+		try (FileOutputStream out = new FileOutputStream(rawHisFile)) {
+			rawHis.storeToXML(out, new Date().toString(), "UTF-8");
+		}
+		if (!wordHisFile.exists()) {
+			wordHisFile.createNewFile();
+		}
+		try (FileOutputStream out = new FileOutputStream(wordHisFile)) {
+			wordHis.storeToXML(out, new Date().toString(), "UTF-8");
+		}
+	}
+}
